f9a8d3cedb65406807f481aa06fde1dd4bc53744
Emilie Allart <emilie.allart@etudiant.univ-lille1.fr>
2015-09-27 11:49:47 +0200

    VND

--- OC/TP1/src/Main.java
+++ OC/TP1/src/Main.java
@@ -6,25 +6,41 @@ public class Main {
 	public static void main(String[] args) {
 		Parser parser = new Parser();
 		Instance[] instances = parser.parse("wt100.txt", 100) ;
-		ArrayList<ArrayList<int[]>> list = new ArrayList<ArrayList<int[]>>();
-		ArrayList<int[]> tmp = new ArrayList<int[]>();
-		ArrayList<Integer> cost = new ArrayList<Integer>();
+//		ArrayList<ArrayList<int[]>> list = new ArrayList<ArrayList<int[]>>();
+//		ArrayList<int[]> tmp = new ArrayList<int[]>();
+//		ArrayList<Integer> cost = new ArrayList<Integer>();
 		HillClimbing hillC = new HillClimbing("SWAP", false, "EDD");
-//		int i = 0; 
-		long t0 = System.currentTimeMillis();
+		
+		String[] neighbor = {"EXCHANGE","SWAP","INSERT"};
+		VND vnd = new VND(neighbor, false, "EDD");
+		
+		long t,t1,t2;
+		/**
+		int i = 0; 
+		
 		
 		
 		for (Instance inst : instances) {
-//			t = System.currentTimeMillis();
-			tmp = hillC.launch(inst);
-			list.add(tmp);
-			cost.add(Scheduler.cost(tmp));
-//			System.out.println("i : " + i + " " + (System.currentTimeMillis()-t) );
-//			i++;
+			t = System.currentTimeMillis();
+			tmp = vnd.launch(inst);
+			//list.add(tmp);
+			//cost.add(Scheduler.cost(tmp));
+			System.out.println("i : " + i + " " + (System.currentTimeMillis()-t) );
+			i++;
 		} 
-		System.out.println(System.currentTimeMillis() - t0);
-//		ArrayList<int[]> tmp = hillC.launch(instances[1]);
-//		System.out.println("Result : "+ Scheduler.cost(tmp));
+		**/
+		t = System.currentTimeMillis();
+		ArrayList<int[]> tmp1 = hillC.launch(instances[40]);
+		t1 = System.currentTimeMillis() - t;
+		
+		System.currentTimeMillis();
+		ArrayList<int[]> tmp2 = vnd.launch(instances[40]);
+		t2 = System.currentTimeMillis() - t;
+		
+		System.out.println("Result HillClimbing: "+ Scheduler.cost(tmp1) + " in " + t1 + " msec");
+		System.out.println("Result VND: "+ Scheduler.cost(tmp2) + " in " + t2 + " msec");
+
+
 	}
 }
 
---
+++ OC/TP1/src/PipedVND.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+
+
+public class PipedVND {
+	private String[] neighbor = {"EXCHANGE","SWAP","INSERT"}; // || SWAP || EXCHANGE
+	private boolean best = false; // || BEST
+	private String init = "RDM"; // EDD || MDD
+	private int k = neighbor.length;
+	
+	public PipedVND(String[] neigth, boolean best, String ini) {
+		this.neighbor = neigth;
+		this.best = best;
+		this.init = ini;
+	}
+	
+	public ArrayList<int[]> launch (Instance inst) {
+		// INIT
+		ArrayList<int[]> jobs = Scheduler.schedule(inst.jobs, this.init);
+		int i = 0;
+		int cost = Scheduler.cost(jobs);
+		int cost_t = 0;
+		ArrayList<int[]> tmp = null;
+		
+		while (i < k) {
+			// faire un rdm sur i et ajouter un compteur
+			tmp = Selector.select(jobs, neighbor[i], this.best);
+			cost_t = Scheduler.cost(tmp);
+			
+			if (cost_t < cost) {
+				cost = cost_t;
+				jobs = new ArrayList<int[]>(tmp);
+			} else {
+				i++;
+			}
+		}
+		return jobs;
+	}
+}
