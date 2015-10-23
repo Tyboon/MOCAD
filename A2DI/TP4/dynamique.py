204f11a79f4ef8e6b807649a6833f6c483c7f3b3
Allart Emilie <allarte@b04p3.fil.univ-lille1.fr>
2015-10-23 17:40:02 +0200

    r-pi

--- A2DI/TP4/dynamique.py
+++ A2DI/TP4/dynamique.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-#A, A_p, B, B_p
+A = 1, B = 3, A_p = 21, B_p = 18
 
 def init() : 
     pass
 
 
-def get_r_pi() :
+def get_r_pi(pi = 0.25) :
     '''
     Récompense moyenne immédiate
     param : S tableau de state
@@ -17,20 +17,38 @@ def get_r_pi() :
     for (i in np.range(25)) :
         cum = 0
         # pour chaque action
-        for (a in np.range(4)) :
-            s = 0
-            # pour chaque etat d'arrivee s_p
-            for (j in np.range(25)) :
-                t = 0
-                if (move(i, a) == j)
-                    t = 1
-                r = rec(i,j,a)
-                s += t * r
-            cum += 0.25 * s
+        for (a in np.range(4)) :        
+            cum += pi * cost(i, a)
         r_pi.append(cum)
     return r_pi
 
 
-def move(s,a) :
-    
+def cost(s,a) :
+    if (x * y < 0 || x >= 5 || y >= 5) :
+        return -1
+    if ( x*5 + y == A ) :
+        return 10
+    if (x*5+y == B :
+        return 5
+    return 0
 
+        
+
+def move(s,a) :
+    '''
+    Renvoie les coordonnees d'arrivee d'apres un mouvement et un etat de depart
+    '''
+    if (a == 0) :
+        x = (s/5)
+        y = (s mod 5) - 1
+    elif (a == 1) :
+        x = s/5
+        y = (s mod y) + 1
+    elif (a == 2) :
+        x = s/5 - 1
+        y = (s mod 5) 
+    else :
+        x = s/5 + 1
+        y = (s mod 5)
+
+    return x, y
