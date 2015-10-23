b6ce2dd070fd9881773818e69329570f5cdce2b0
Emilie Allart <emilie.allart@etudiant.univ-lille1.fr>
2015-10-23 23:54:52 +0200

    r-pi ok

--- A2DI/TP4/dynamique.py
+++ A2DI/TP4/dynamique.py
@@ -1,38 +1,30 @@
 import numpy as np
 
-A = 1, B = 3, A_p = 21, B_p = 18
+pt_a = 1
+pt_b = 3 
+pt_a_p = 21
+pt_b_p = 18
 
 def init() : 
     pass
 
 
 def get_r_pi(pi = 0.25) :
-    '''
-    Récompense moyenne immédiate
-    param : S tableau de state
-    '''
-    
-    r_pi = []
-    # pour chaque etat de depart x = i/5 y = i mod 5
-    for (i in np.range(25)) :
-        cum = 0
-        # pour chaque action
-        for (a in np.range(4)) :        
-            cum += pi * cost(i, a)
-        r_pi.append(cum)
-    return r_pi
+	'''
+	Recompense moyenne immediate
+	param : S tableau de state
+	'''
+	r_pi = []
+	# pour chaque etat de depart x = i/5 y = i mod 5
+	for i in range(25) :
+		cum = 0
+		# pour chaque action
+		for a in range(4) :
+			cum += pi * cost(i, a)
+		r_pi.append(cum)
+		print cum
+	return r_pi
 
-
-def cost(s,a) :
-    if (x * y < 0 || x >= 5 || y >= 5) :
-        return -1
-    if ( x*5 + y == A ) :
-        return 10
-    if (x*5+y == B :
-        return 5
-    return 0
-
-        
 
 def move(s,a) :
     '''
@@ -40,15 +32,28 @@ def move(s,a) :
     '''
     if (a == 0) :
         x = (s/5)
-        y = (s mod 5) - 1
+        y = (s % 5) - 1
     elif (a == 1) :
         x = s/5
-        y = (s mod y) + 1
+        y = (s % 5) + 1
     elif (a == 2) :
         x = s/5 - 1
-        y = (s mod 5) 
+        y = (s % 5) 
     else :
         x = s/5 + 1
-        y = (s mod 5)
+        y = (s % 5)
 
     return x, y
+
+
+def cost(s,a) :
+	x, y = move(s,a)
+	if (x * y < 0 or x >= 5 or y >= 5) :
+		return -1
+	if ( (x*5 + y) == pt_a ) :
+		return 10
+	if ( (x*5+y) == pt_b) :
+		return 5
+	return 0
+
+
