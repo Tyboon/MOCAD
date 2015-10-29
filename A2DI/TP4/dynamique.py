27065d0e7c1a0ca104e1d889e67535aafd756f4e
Emilie Allart <emilie.allart@etudiant.univ-lille1.fr>
2015-10-29 17:48:45 +0100

    fin tp !!!

--- A2DI/TP4/dynamique.py
+++ A2DI/TP4/dynamique.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 
 pt_a = 1
 pt_b = 3 
@@ -9,36 +10,36 @@ def init() :
     pass
 
 def get_v(alpha = 0.9) :
-    r_pi = get_r_pi()
-    p_pi = get_p_pi()
-    ident = np.eye(len(p_pi),len(p_pi))
-    return np.transpose(ident - alpha * p_pi) * r_pi
+	r_pi = get_r_pi()
+	p_pi = get_p_pi()
+	ident = np.identity(len(p_pi))
+	return np.dot(inv(ident - np.dot(alpha, p_pi)), r_pi)
 
 def get_p_pi(pi = 0.25) :
-    p = np.zeros((25,25))
-    for s in range(25) :
-        for s_new in range(25) :
-            tmp = 0
-            for a in range(4) :
-                print s, s_new, a
-                m_x, m_y = move(s,a)
-                if m_x < 0 :
-                    m_x += 1
-                elif m_x >= 5 :
-                    m_x -= 1
-                if m_y < 0 :
-                    m_y += 1
-                elif m_x >= 5 :
-                    m_y -= 1
-
-                if (m_x * 5 + m_y) == s_new :
-                   tmp += pi * 1
-            p[s,s_new] = tmp
-
-    p[pt_a,pt_a_p] = 1
-    p[pt_b,pt_b_p] = 1
-    return p
-            
+	p = np.zeros((25,25))
+	for s in range(25) :
+		if s == pt_a :
+			p[s, pt_a_p] = 1
+		elif s == pt_b :
+			p[s,pt_b_p] = 1
+		else :
+			for s_new in range(25) :
+				tmp = 0
+				for a in range(4) :
+					m_x, m_y = move(s,a)
+					if m_x < 0 :
+					  m_x += 1
+					elif m_x >= 5 :
+					  m_x -= 1
+					if m_y < 0 :
+					  m_y += 1
+					elif m_x >= 5 :
+						m_y -= 1
+					if (m_x * 5 + m_y) == s_new :
+						tmp += pi * 1
+				p[s,s_new] = tmp
+	return p
+			
 
 def get_r_pi(pi = 0.25) :
 	'''
@@ -53,7 +54,6 @@ def get_r_pi(pi = 0.25) :
 		for a in range(4) :
 			cum += pi * cost(i, a)
 		r_pi.append(cum)
-		print cum
 	return r_pi
 
 
