d9288130b42e9b4ddf35f4036a864643f84b6448
Emilie Allart <emilie.allart@etudiant.univ-lille1.fr>
2015-12-15 09:44:43 +0100

    tp1(

--- A2DI/part2/TP1/PCA.py
+++ A2DI/part2/TP1/PCA.py
@@ -3,6 +3,7 @@ from scipy import linalg
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def parse (filename) :
     matrix, c = [], []
@@ -24,10 +25,50 @@ def main() :
     
     val, U = linalg.eig(R)
     # 1D 
-    Y_1 = np.dot(X_, U[:,0])
-    plt.plot(Y_1[0], np.zeros(n))
-    # 2D
+    tot = sum(val[:1]) / p
+    print "Pourcentage de variance totale : %f" % tot
+    Y_1 = np.dot(X_, U[:,:1])
+    plt.plot(Y_1[:50,0], np.zeros(50), 'ro')
+    plt.plot(Y_1[50:100,0], np.zeros(50), 'bo')
+    plt.plot(Y_1[100:150,0], np.zeros(50), 'go')
+    plt.title('1D')
+    plt.show()
+    plt.clf()
+
+    err = np.mean((np.absolute(R - np.dot(Y_1, U[:,:1].T))))
+    print " Erreur absolue moyenne : %f" % err  
+	 
+	 
+	 # 2D
+    tot = sum(val[:2]) / p
+    print "Pourcentage de variance totale : %f" % tot
     Y_2 = np.dot(X_, U[:,:2])
-    # 3D
+    plt.plot(Y_2[:50,0],Y_2[:50,1], 'ro')
+    plt.plot(Y_2[50:100,0],Y_2[50:100,1], 'bo')
+    plt.plot(Y_2[100:150,0],Y_2[100:150,1], 'go')
+    plt.title('2D')
+    plt.show()
+    plt.close()
+    
+    err = np.mean((np.absolute(R - np.dot(Y_2, U[:,:2].T))))
+    print " Erreur absolue moyenne : %f"%err
+
+    
+	 # 3D
+    tot = sum(val[:3]) / p
+    print "Pourcentage de variance totale : %f" % tot
     Y_3 = np.dot(X_, U[:,:3])
-    return Y_
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(np.asarray(Y_3[:50,0]),np.asarray(Y_3[:50,1]),np.asarray(Y_3[:50,2]),color='r',marker='o')
+    ax.scatter(np.asarray(Y_3[50:100,0]),np.asarray(Y_3[50:100,1]),np.asarray(Y_3[50:100,2]),color='b',marker='o')
+    ax.scatter(np.asarray(Y_3[100:150,0]),np.asarray(Y_3[100:150,1]),np.asarray(Y_3[100:150,2]),color='g',marker='o')
+    plt.title('3D')
+    plt.show()
+    
+    err = np.mean((np.absolute(R - np.dot(Y_3, U[:,:3].T))))
+    print " Erreur absolue moyenne : %f"%err
+    return 
+
+if __name__ == '__main__' :
+    main()
