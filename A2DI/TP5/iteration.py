313428aacc8890e192c524045121b7b782293edb
Allart Emilie <allarte@b04p3.fil.univ-lille1.fr>
2015-12-16 19:36:34 +0100

    fin SV tp2 et PCA

---
+++ A2DI/TP5/iteration.py
@@ -0,0 +1,84 @@
+import numpy as np
+from numpy.linalg import inv
+
+pt_a = 1
+pt_b = 3
+pt_a_p = 21
+pt_b_p = 13
+
+def init() :
+    pass
+
+def get_v(alpha = 0.9) :
+    r_pi = get_r_pi()
+    p_pi = get_p_pi()
+    ident = np.identity(len(p_pi))
+    return np.dot(inv(ident - np.dot(alpha, p_pi)), r_pi)
+
+def get_p_pi(pi = 0.25) :
+    p = np.zeros((25,25))
+    for s in range(25) :
+        if s == pt_a :
+            p[s, pt_a_p] = 1
+        elif s == pt_b :
+            p[s,pt_b_p] = 1
+        else :
+            for s_new in range(25) :
+                tmp = 0
+                for a in range(4) :
+                    m_x, m_y = move(s,a)
+                    if m_x < 0 :
+                        m_x += 1
+                    elif m_x >= 5 :
+                        m_x -= 1
+                    if m_y < 0 :
+                        m_y += 1
+                    elif m_x >= 5 :
+                        m_y -= 1
+                    if (m_x * 5 + m_y) == s_new :
+                        tmp += pi * 1
+                p[s,s_new] = tmp
+    return p
+
+def get_r_pi(pi = 0.25) :
+    '''
+    Recompense moyenne immediate
+    param : S tableau de state
+    '''
+    r_pi = []
+    # pour chaque etat de depart x = i/5 y = i mod 5
+    for i in range(25) :
+        cum = 0
+        # pour chaque action
+        for a in range(4) :
+            cum += pi * cost(i, a)
+            r_pi.append(cum)
+    return r_pi
+
+def move(s,a) :
+    '''
+    Renvoie les coordonnees d'arrivee d'apres un mouvement et un etat de depart
+    '''
+    if (a == 0) :
+        x = (s/5) - 1
+        y = (s % 5)
+    elif (a == 1) :
+        x = s/5 + 1
+        y = (s % 5)
+    elif (a == 2) :
+        x = s/5
+        y = (s % 5) - 1
+    else :
+        x = s/5
+        y = (s % 5) + 1
+    return x, y
+
+def cost(s,a) :
+    if (s == pt_a) :
+        return 10
+    if (s == pt_b) :
+        return 5
+    x, y = move(s,a)
+    if (x < 0 or y <0 or x >= 5 or y >= 5) :
+        return -1
+    return 0
---
+++ A2DI/part2/Allart_PCA/PCA.py
@@ -0,0 +1,77 @@
+from numpy import mean, std, cov
+from scipy import linalg
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def parse (filename) :
+    matrix, c = [], []
+    with open(filename,'r') as f :
+        reader = csv.reader(f, delimiter=",")
+        for row in reader :
+            r = map(float, row[:-1])
+            c.append(row[-1])
+            matrix.append(r)
+    return np.asmatrix(matrix), c 
+
+def main() :
+    X, c = parse('iris.data')
+    n,p = X.shape
+    # Reduction de X
+    X_ = (X - np.mean(X,axis=0)) / np.std(X,axis=0)
+    
+    R = 1./n * np.dot(X_.T,X_) 
+    
+    val, U = linalg.eig(R)
+    # 1D 
+    tot = sum(val[:1]) / p
+    print "Pourcentage de variance totale : {0}".format(tot)
+    Y_1 = np.dot(X_, U[:,:1])
+    plt.plot(Y_1[:50,0], np.zeros(50), 'ro')
+    plt.plot(Y_1[50:100,0], np.zeros(50), 'bo')
+    plt.plot(Y_1[100:150,0], np.zeros(50), 'go')
+    plt.title('1D')
+    plt.show()
+    plt.clf()
+
+    # Erreur de reconstruction a partir de la matrice reduite a 1D  
+    err = np.mean((np.absolute(X_ - np.dot(Y_1, U[:,:1].T))))
+    print " Erreur absolue moyenne : %f" % err  
+	 
+	 
+	 # 2D
+    tot = sum(val[:2]) / p
+    print "Pourcentage de variance totale : %f" % tot
+    Y_2 = np.dot(X_, U[:,:2])
+    plt.plot(Y_2[:50,0],Y_2[:50,1], 'ro')
+    plt.plot(Y_2[50:100,0],Y_2[50:100,1], 'bo')
+    plt.plot(Y_2[100:150,0],Y_2[100:150,1], 'go')
+    plt.title('2D')
+    plt.show()
+    plt.close()
+
+    # Erreur de reconstruction a partir de la matrice reduite a 2D  
+    err = np.mean((np.absolute(X_ - np.dot(Y_2, U[:,:2].T))))
+    print " Erreur absolue moyenne : %f"%err
+
+    
+	 # 3D
+    tot = sum(val[:3]) / p
+    print "Pourcentage de variance totale : %f" % tot
+    Y_3 = np.dot(X_, U[:,:3])
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(np.asarray(Y_3[:50,0]),np.asarray(Y_3[:50,1]),np.asarray(Y_3[:50,2]),color='r',marker='o')
+    ax.scatter(np.asarray(Y_3[50:100,0]),np.asarray(Y_3[50:100,1]),np.asarray(Y_3[50:100,2]),color='b',marker='o')
+    ax.scatter(np.asarray(Y_3[100:150,0]),np.asarray(Y_3[100:150,1]),np.asarray(Y_3[100:150,2]),color='g',marker='o')
+    plt.title('3D')
+    plt.show()
+    
+    # Erreur de reconstruction a partir de la matrice reduite a 3D
+    err = np.mean((np.absolute(X_ - np.dot(Y_3, U[:,:3].T))))
+    print " Erreur absolue moyenne : %f"%err
+    return 
+
+if __name__ == '__main__' :
+    main()
