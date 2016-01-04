16f5c638a9b29d589bdd2dec265ad8ff82b68543
Emilie Allart <emilie.allart@etudiant.univ-lille1.fr>
2016-01-05 00:33:49 +0100

    tp2 finir visualisation

--- A2DI/part2/TP2/SVD.py
+++ A2DI/part2/TP2/SVD.py
@@ -1,7 +1,22 @@
-import numpy as np 
+import numpy as np
+from scipy import linalg
 
 def similarity(doc, req) :
-    return np.dot(doc.T, req) / (np.linalg.norm(doc) * np.linalg.norm(req))
+    return np.dot(doc, req) / (np.linalg.norm(doc) * np.linalg.norm(req))
+
+def reduce(u, e, v, k=2) :
+	u = u[:,:k]
+	e = e[:k,:k]
+	v = v[:k,:]
+	return u, e, v
+
+def parse(d) :
+	d = d.replace('.', '')
+	d = d.replace(',', '')
+	d = d.replace(':', '')
+	d = d.replace('\'','')
+	d = d.replace('\n','')
+	return d
 
 d1 = "Romeo and Juliet ."
 d2 = "Juliet : O happy dagger ."
@@ -11,16 +26,18 @@ d5 = "Did you know , New-Hampshire is in New England ."
 
 q = "die dagger"
 
-d1 = d1.split()
-d2 = d2.split()
-d3 = d3.split()
-d4 = d4.split()
-d5 = d5.split()
+d1 = parse(d1).split()
+d2 = parse(d2).split()
+d3 = parse(d3).split()
+d4 = parse(d4).split()
+d5 = parse(d5).split()
 
 doc = [d1, d2, d3, d4, d5]
 q = q.split()
 terms = list(set(d1+d2+d3+d4+d5))
 term_doc, term_q = [], []
+
+print terms
 
 for w in terms :
     l = []
@@ -30,9 +47,31 @@ for w in terms :
     term_doc.append(l)
     term_q.append(q.count(w))
 
-print term_doc[:][0]
+term_doc = np.asarray(term_doc)
+
+print term_q
+print len(term_q)
+
 simil = []
 for i in range(5) :
     simil.append(similarity(term_doc[:,i], term_q))
-
 print simil
+
+u,e,v = linalg.svd(term_doc) 
+e = np.diag(e)
+
+u,e,v = reduce(u,e,v,2)
+
+#print u, e, v 
+
+d_k = np.dot(e, v)
+
+q_k = np.dot(np.dot(np.linalg.inv(e), u.T), term_q) 
+
+simil2 =[]
+for i in range(5) :
+	simil2.append(similarity(d_k[:,i], q_k))
+print simil2
+
+
+
