3455dd85601eaea9efc35272ecfca73922a1c1ce
Allart Emilie <allarte@b04p3.fil.univ-lille1.fr>
2016-01-13 17:21:57 +0100

    twitter

--- Twitter/src/Streaming.java
+++ Twitter/src/Streaming.java
@@ -1,4 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import twitter4j.StallWarning;
 import twitter4j.Status;
@@ -10,12 +17,54 @@ import twitter4j.TwitterStreamFactory;
 
 
 public class Streaming {
-
+	
+	public static List<String> tweets = new ArrayList<String>();
+	public static Writer writer = null ;
 	
 	public static void main(String[] args) throws TwitterException, IOException{
+		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("dico.txt"),"utf-8"));
 	    StatusListener listener = new StatusListener() {
-	        public void onStatus(Status status) {
-	            System.out.println(status.getText() + '\n');
+	        @SuppressWarnings("resource")
+			public void onStatus(Status status) {
+	            String st = status.getText() + "\n";
+	            Scanner sc = new Scanner(System.in);
+	            System.out.println(st + " ?");
+	            String s = sc.nextLine();
+	            switch (s) {
+	            case "+" :
+	            	try {
+						writer.write("+ " + st);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	            	break;
+	            case "-" :
+	            	try {
+						writer.write("- " + st);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	            	break;
+	            case "_" :
+	            	try {
+						writer.write("_ " + st);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	            	break;
+	            case "x" :
+	            	try {
+						writer.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	            default :
+	            	break;
+	            }
 	        }
 	        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
 	        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
