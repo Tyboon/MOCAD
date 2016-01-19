ba4cbc45884483d3df0ba52cabdc7559380a75cb
Allart Emilie <allarte@b04p9.fil.univ-lille1.fr>
2016-01-19 17:11:12 +0100

    maj

--- A2DI/part2/TP4/tp4.py
+++ A2DI/part2/TP4/tp4.py
@@ -1,8 +1,47 @@
 import numpy as np
+from math import exp, pow, fabs
+import csv
 
-def e_graph(data, len_data, sigma) :
+epsilon = 1.0
+sigma = 1.0
+
+'''
+    Retourne X et Y
+'''
+def parse (filename) :
+    matrix, c = [], []
+    with open(filename,'r') as f :
+        reader = csv.reader(f, delimiter=",")
+        for row in reader :
+            r = map(float, row[:-1])
+            c.append(row[-1])
+            matrix.append(r)
+    return np.asmatrix(matrix), c 
+
+
+def similarity (xi, xj) :
+    exp(- np.linalg.norm((data[xi]-data[xj]), 2)/pow(sigma,2))
+
+
+'''
+    Matrice de similarite
+'''
+def e_graph(data, len_data) :
     s = np.zeros((len_data, len_data))
     for i in range(len_data) :
         for j in range (len_data) :
-            s[i,j] = exp(- norm(data[i]-data[j])/pow(sigma,2))
+            if (similarity (i,j) < epsilon) :
+                s[i,j] = 1
     return s
+
+
+def main() :
+    X,Y = parse('iris.data')
+    n,p = X.shape
+    graph = e_graph(X, n)
+    D = []
+
+
+
+if __name__ == '__main__' :
+    main()
